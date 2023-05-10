01f4b3e9fc78b9d86ac4a0d524c2bcc261a98f80
MShverdiakov <94805834+MShverdiakov@users.noreply.github.com>
2023-05-10 19:21:44 +0300

    Add files via upload

---
+++ Lab6/Lab6/Car.h
@@ -0,0 +1,39 @@
+class Car {
+public:
+    int number, cost, brand;
+    Car() {
+        number = 0;
+        cost = 0;
+        brand = 0;
+    }
+    Car(const Car& s) {                                            //êîíñòğóêòîğ êîïèğîâàíèÿ
+        this->number = s.number;
+        this->cost = s.cost;
+        this->brand = s.brand;
+    }
+    friend istream& operator>>(istream& in, Car& a) {
+        cout << "Ââåäèòå íîìåğ:" << endl;
+        in >> a.number;
+        cout << "âûáåğèòå ìàğêó (0 - ÊÀÌÀÇ, 1 - ÁåËÀÇ, 2 - ÃÀÇ-24, 3 - Èêàğóñ, 4 - ËÈÀÇ, 5 - ĞÀÔ):" << endl;
+        in >> a.brand;
+        if ((a.brand < 0) || (a.brand > 5)) { cout << "íåâåğíàÿ öèôğà"; exit; }
+        cout << "Ââåäèòå ñòîèìîñòü:" << endl;
+        in >> a.cost;
+        return (in);
+    }
+
+    friend ostream& operator<<(ostream& out, Car a) {
+        out << a.number << " ";
+        string brands[6] = { "ÊÀÌÀÇ", "ÁåËÀÇ", "ÃÀÇ-24", "Èêàğóñ", "ËÈÀÇ", "ĞÀÔ" };
+        cout << brands[a.brand];
+        cout << " " << a.cost;
+        return (out);
+    }
+
+    bool operator==(Car& a) {
+        if ((a.number == this->number) && (a.cost == this->cost) && (a.brand == this->brand)) {
+            return true;
+        }
+        return false;
+    }
+};
---
+++ Lab6/Lab6/Lab6.cpp
@@ -0,0 +1,45 @@
+﻿#include "Set.h"
+#include "Car.h"
+
+void solution(Set<int>* brands_of_bases, Set<Car>* bases, int num_of_bases) {
+    bool flag = false;
+    for (int i = 0; i < num_of_bases; i++) {
+        for (int j = 0; (j < num_of_bases) && (i != j); j++) {
+            if (brands_of_bases[i] == brands_of_bases[j]) {
+                cout << j << "-ая автобаза";
+                cout << bases[j] << endl;
+                cout << i << "-ая автобаза";
+                cout << bases[i] << endl;
+                int sum = 0;
+                for (int k = 0; k < bases[i].size; k++) sum += bases[i].base[k].cost + bases[j].base[k].cost;
+                cout << "сумма " << sum;
+                flag = true;
+            }
+        }
+    }
+    if (!flag) cout << "нет одинаковых";
+}
+
+int main() {
+
+    setlocale(LC_ALL, "ru");
+
+    int num_of_bases, num_of_cars;
+    Car car;
+
+    cout << "введите кол-во автобаз" << endl;
+    cin >> num_of_bases;
+    Set<int>* brands_of_bases = new Set<int>[num_of_bases];
+    Set<Car>* bases = new Set<Car>[num_of_bases];
+    for (int i = 0; i < num_of_bases; i++) {
+        cout << "введите кол-во машин на " << i << " автобазе"; cin >> num_of_cars;
+        cout << "введите машины (номер, марка, стоимость)";
+        for (int j = 0; j < num_of_cars; j++) {
+            cin >> car.number >> car.brand >> car.cost;
+            bases[i].add_elem(car);
+            brands_of_bases[i].add_elem(car.brand);
+        }
+    }
+    
+    solution(brands_of_bases, bases, num_of_bases);
+}
